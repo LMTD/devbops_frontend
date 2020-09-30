a137767b2f3a8406c12fd65b86cbb45c5ff94ea0
Kai Hang Chen <kaihang.c@gmail.com>
2020-09-29 20:52:32 -0400

    set up the routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Login from './containers/auth/login/Login';
+import SignUp from './containers/auth/signUp/SignUp';
+import Home from './containers/home/Home';
+import EventDetail from './containers/eventDetail/EventDetail';
 
 function App() {
-	return <div></div>;
+	return (
+		<Switch>
+			<Route exact path={('/', '/home')} component={Home} />
+			<Route path='/login' component={Login} />
+			<Route path='/sign-up' component={SignUp} />
+			<Route exact path='/event-detail/:id' component={EventDetail} />
+		</Switch>
+	);
 }
 
 export default App;
---
+++ src/containers/auth/signUp/SignUp.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignUp = () => {
+	return <div>This is sign up page</div>;
+};
+
+export default SignUp;
