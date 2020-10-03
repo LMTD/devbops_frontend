49b8c7a2deab1e46df034ae5e309fe0523e9a59f
Kai Hang Chen <kaihang.c@gmail.com>
2020-10-03 10:44:17 -0400

    add alert components

--- src/containers/auth/login/Login.jsx
+++ src/containers/auth/login/Login.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { useForm } from 'react-hook-form';
+
 import {
 	Container,
 	Typography,
@@ -7,14 +9,41 @@ import {
 	Button,
 	Grid,
 } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
 
 const Login = () => {
-	const { register, handleSubmit, errors } = useForm();
-	const submitLoginForm = async (data) => {
-		console.log(data);
+	const [alertSeverity, setAlertSeverity] = useState('');
+	const [alertMessage, setAlertMessage] = useState('');
+
+	const { register, handleSubmit, errors, reset } = useForm();
+	const submitLoginForm = async (formData) => {
+		try {
+			const { data } = await axios.post(
+				'https://0c77865x10.execute-api.us-east-1.amazonaws.com/v1/user',
+				{
+					Action: 'login',
+					Username: formData.username,
+					Password: formData.password,
+				},
+			);
+			console.log('this is data: ', data);
+			if (data.statusCode === 200) {
+				setAlertSeverity('success');
+				setAlertMessage('Login Successfully');
+				reset();
+			} else {
+				setAlertSeverity('error');
+				setAlertMessage(data.Error);
+			}
+		} catch (err) {
+			console.log('there is an error in login: ', err);
+			setAlertSeverity('error');
+			setAlertMessage('Network error');
+		}
 	};
 	return (
 		<Container fixed>
+			{alertMessage && <Alert severity={alertSeverity}>{alertMessage}</Alert>}
 			<Typography component='h1' variant='h5'>
 				Login
 			</Typography>
@@ -40,7 +69,7 @@ const Login = () => {
 					label='Password'
 					type='password'
 					id='password'
-					inputRef={register({ required: true, minLength: 8 })}
+					inputRef={register({ required: true })}
 					error={errors.password?.type === 'required'}
 				/>
 				<Grid container justify='center'>
