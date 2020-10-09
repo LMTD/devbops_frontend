a1cd0af4b0c7ef554b74522df323e444385c6c8a
Kai Hang Chen <kaihang.c@gmail.com>
2020-10-08 23:11:33 -0400

    add dummy data on the authenticated  home page

--- src/components/UI/slideShow/SlideShow.jsx
+++ src/components/UI/slideShow/SlideShow.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import Carousel from 'react-elastic-carousel';
+import SlideItem from './slideItem/SlideItem';
 
-const SlideShow = () => {
+import './SlideShow.css';
+
+const SlideShow = (props) => {
+	// console.log('this is props: ', props);
+	const items = props.slideItems.map((item) => (
+		<SlideItem
+			key={item.title}
+			title={item.title}
+			description={item.description}
+			imageUrl={item.imgUrl}
+		/>
+	));
+
 	return (
-		<Carousel itemsToShow={4} pagination={false} style={{ margin: '10px 0' }}>
-			<div>this is one</div>
-			<div>this is one1</div>
-			<div>this is one2</div>
-			<div>this is one3</div>
-			<div>this is one4</div>
-			<div>this is one5</div>
+		<Carousel itemsToShow={4} pagination={false}>
+			{items}
 		</Carousel>
 	);
 };
